b074d92bc8f70d5542359fc08f8ddba27039d4ff
LunarDiver <25Prozent@gmail.com>
2021-04-26 14:00:53 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiMovementDetector
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
