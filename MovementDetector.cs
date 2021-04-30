7f1550e76a3733109a4079197496f844881fd607
LunarDiver <25Prozent@gmail.com>
2021-04-30 12:28:11 +0200

    First implementation.

---
+++ MovementDetector.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Drawing;
+
+namespace PiMovementDetector
+{
+    internal class MovementDetector
+    {
+        public byte MovementThreshold { get; set; } = 64;
+
+        public double DetectionPercentMin { get; set; } = 0.1;
+
+        public MovementDetector(byte movementThreshold = 64, double detectionPercentMin = 0.1)
+        {
+            MovementThreshold = movementThreshold;
+            DetectionPercentMin = detectionPercentMin;
+        }
+
+        public bool HasMovement(Bitmap prevImage, Bitmap currImage)
+        {
+            if (prevImage.Width != currImage.Width || prevImage.Height != currImage.Height)
+                throw new ArgumentException("Images must be of the same size.");
+
+            using var movementImage = GetMovementImage(prevImage, currImage, MovementThreshold);
+
+            return GetMovementPercent(movementImage) >= DetectionPercentMin;
+        }
+
+        public double GetMovementPercent(Bitmap prevImage, Bitmap currImage)
+        {
+            using var movementImage = GetMovementImage(prevImage, currImage, MovementThreshold);
+            return GetMovementPercent(movementImage);
+        }
+
+        private static double GetMovementPercent(Bitmap movementImage)
+        {
+            int totalPixels = movementImage.Width * movementImage.Height;
+            int movingPixels = 0;
+
+            for (int x = 0; x < movementImage.Width; x++)
+                for (int y = 0; y < movementImage.Height; y++)
+                {
+                    Color currPixel = movementImage.GetPixel(x, y);
+
+                    if (currPixel != Color.Black)
+                        movingPixels++;
+                }
+
+            return (double)movingPixels / totalPixels;
+        }
+
+        private static Bitmap GetMovementImage(Bitmap prev, Bitmap curr, byte threshold)
+        {
+            var newImg = new Bitmap(curr.Width, curr.Height);
+
+            for (int x = 0; x < newImg.Width; x++)
+                for (int y = 0; y < newImg.Height; y++)
+                {
+                    Color lastPixel = prev.GetPixel(x, y);
+                    Color currPixel = curr.GetPixel(x, y);
+
+                    int newR = lastPixel.R - currPixel.R;
+                    int newG = lastPixel.G - currPixel.G;
+                    int newB = lastPixel.B - currPixel.B;
+
+                    if (newR < 64)
+                        newR = 0;
+                    if (newG < 64)
+                        newG = 0;
+                    if (newB < 64)
+                        newB = 0;
+
+                    newImg.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+                }
+
+            return newImg;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,97 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Camera;
+using System.Timers;
+using System;
+using System.IO;
+using System.Drawing;
 
 namespace PiMovementDetector
 {
     internal class Program
     {
+        private static Timer _picTaker = new Timer
+        {
+            AutoReset = true,
+            Interval = 5000
+        };
+
+        private static Timer _picProcesser = new Timer
+        {
+            AutoReset = true,
+            Interval = 500
+        };
+
+        private static readonly object _picLock = new object();
+
+        private static readonly Queue<(DateTime taken, Bitmap image)> _imgData = new Queue<(DateTime taken, Bitmap image)>();
+
+        private static (DateTime taken, Bitmap image) _lastImg;
+
+        private static MovementDetector _detector = new MovementDetector();
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Directory.CreateDirectory("imgs");
+
+            _picTaker.Elapsed += (_, _) => TakePicture();
+            _picProcesser.Elapsed += (_, _) => ProcessPictureQueueNew();
+            _picTaker.Start();
+            _picProcesser.Start();
+
+            Console.ReadLine();
+
+            _picTaker.Dispose();
+            _picProcesser.Dispose();
+
+            _lastImg.image?.Dispose();
+            while (_imgData.TryDequeue(out (DateTime taken, Bitmap image) disposingImg))
+                disposingImg.image.Dispose();
+        }
+
+        private static void TakePicture()
+        {
+            using var imgStream = new MemoryStream();
+            lock (_picLock)
+            {
+                imgStream.Write(Pi.Camera.CaptureImage(new CameraStillSettings
+                {
+                    CaptureEncoding = CameraImageEncodingFormat.Png,
+                    CaptureWidth = 256,
+                    CaptureHeight = 256,
+                    CaptureTimeoutMilliseconds = 500,
+                    ImageContrast = 100,
+                    ImageSaturation = -100,
+                    CaptureDisplayPreview = false,
+                    CaptureWhiteBalanceControl = CameraWhiteBalanceMode.Cloud,
+                    CaptureVideoStabilizationEnabled = false
+                }));
+            }
+
+            var img = new Bitmap(imgStream);
+
+            _imgData.Enqueue((DateTime.UtcNow, img));
+        }
+
+        private static void ProcessPictureQueueNew()
+        {
+            if (_imgData.Count == 0)
+                return;
+
+            if (_lastImg == default)
+            {
+                _lastImg = _imgData.Dequeue();
+                return;
+            }
+
+            var currImg = _imgData.Dequeue();
+
+            double movement = _detector.GetMovementPercent(_lastImg.image, currImg.image);
+
+            Console.WriteLine($"[{currImg.taken}]: {Math.Round(movement * 100, 2)}%");
+
+            _lastImg.image.Dispose();
+            _lastImg = currImg;
         }
     }
 }
