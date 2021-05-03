befe135eb50b9315d8c66da99c7da986eef5d8ce
LunarDiver <25Prozent@gmail.com>
2021-05-03 10:17:53 +0200

    Allowed multiple tcp connections, added write methods to write to all clients at once.

--- PiMovementDetector/TcpConnector.cs
+++ PiMovementDetector/TcpConnector.cs
@@ -1,39 +1,115 @@
+using System.Timers;
+using System.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace PiMovementDetector
 {
     public class TcpConnector : IDisposable
     {
-        public bool Connected { get; private set; }
-
         private readonly TcpListener _listener;
 
-        private TcpClient _connectedClient;
+        private readonly List<TcpClient> _connectedClients = new List<TcpClient>();
+
+        private readonly Timer _connector = new Timer
+        {
+            AutoReset = false,
+            Interval = 1000
+        };
 
         public TcpConnector(out ushort port)
         {
             _listener = TcpListener.Create(0);
             _listener.Start();
-            _listener.BeginAcceptTcpClient(ClientAccepted, null);
+
+            _connector.Elapsed += ConnectNewClient;
+            _connector.Start();
 
             port = (ushort)((IPEndPoint)_listener.LocalEndpoint).Port;
         }
 
+        public void Write(params byte[] buffer)
+        {
+            CheckConnections();
+
+            for (int i = 0; i < _connectedClients.Count; i++)
+                _connectedClients[i].GetStream().Write(buffer);
+        }
+
+        public void Write<T>(T data)
+        {
+            if (typeof(T)
+                .GetCustomAttributes(true)
+                .FirstOrDefault(a => a.GetType()
+                .Equals(typeof(SerializableAttribute))) == null)
+                throw new ArgumentException($"Type {nameof(T)} must have attribute {nameof(SerializableAttribute)}.");
+
+            byte[] serialized = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes<T>(data);
+
+            Write(serialized);
+        }
+
+        public async Task WriteAsync(params byte[] buffer)
+        {
+            CheckConnections();
+
+            for (int i = 0; i < _connectedClients.Count; i++)
+                await _connectedClients[i].GetStream().WriteAsync(buffer);
+        }
+
+        public async Task WriteAsync<T>(T data)
+        {
+            if (typeof(T)
+                .GetCustomAttributes(true)
+                .FirstOrDefault(a => a.GetType()
+                .Equals(typeof(SerializableAttribute))) == null)
+                throw new ArgumentException($"Type {nameof(T)} must have attribute {nameof(SerializableAttribute)}.");
+
+            byte[] serialized = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes<T>(data);
+
+            await WriteAsync(serialized);
+        }
+
         public event EventHandler<NetworkStream> ClientConnected;
+
+        private void ConnectNewClient(object sender, ElapsedEventArgs e)
+        {
+            _listener.BeginAcceptTcpClient(ClientAccepted, null);
+        }
 
         private void ClientAccepted(IAsyncResult ar)
         {
-            _connectedClient = _listener.EndAcceptTcpClient(ar);
-            Connected = true;
-            ClientConnected.Invoke(this, _connectedClient.GetStream());
+            _connectedClients.Add(_listener.EndAcceptTcpClient(ar));
+            ClientConnected.Invoke(this, _connectedClients[^1].GetStream());
+
+            _connector.Start();
+        }
+
+        private void CheckConnections()
+        {
+            for (int i = 0; i < _connectedClients.Count; i++)
+            {
+                var currClient = _connectedClients[i];
+                if (!currClient.Connected)
+                {
+                    currClient.Dispose();
+                    _connectedClients.Remove(currClient);
+                    i--;
+                    continue;
+                }
+            }
         }
 
         public void Dispose()
         {
+            _connector.Dispose();
             _listener.Stop();
-            _connectedClient?.Dispose();
+
+            foreach (TcpClient client in _connectedClients)
+                client.Dispose();
         }
     }
 }
